6e2cafece3f80f5e9149eceec1593d3988fc64ab
flar2 <asegaert@gmail.com>
2014-12-01 20:27:00 -0500

    fix compilation warnings
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Conflicts:
    	drivers/cpuidle/cpuidle.c
    
    Change-Id: Ib73e4fbb301bc56193909cf3733aa4922d48aeb9

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -117,7 +117,7 @@ int cpuidle_idle_call(void)
 {
 	struct cpuidle_device *dev = __this_cpu_read(cpuidle_devices);
 	struct cpuidle_driver *drv;
-	int next_state, entered_state;
+	int next_state, entered_state = 0;
 	bool broadcast;
 
 	if (off || !initialized)
